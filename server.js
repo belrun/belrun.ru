a44115ec48e1e9036921dd017544a5f57fa2342c
Artem Zhukov <artzhuchka@gmail.com>
2016-02-19 10:03:01 +0300

    change index page

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 'use strict';
 
 var express = require('express');
+var fs = require('fs');
+
 var app = express();
+
+var html = fs.readFileSync('index.html');
 
 app.get('/', function(req, res) {
 	res.status(200);
 	res.header('Content-Type', 'text/html');
-	res.send('hello world');
+	res.send(html);
 });
 
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
